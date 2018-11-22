5822828c49fef25579f0ec86a88ab52d1b38ef7b
Adrian DC <radian.dc@gmail.com>
2018-11-22 20:21:33 +0530

    rolex: Disable EGL_KHR_gl_colorspace extension declaration
    * New libhwui evolutions now implement the EGL_KHR_gl_colorspace as EglExtensions.glColorSpace to apply additional attributes, however the new default attributes are refused by the legacy Adreno 225 EGL binaries and assert with EGL_BAD_ATTRIBUTE
    * Hex-edit EGL_KHR_gl_colorspace to DIS_ABL_ED_colorspace, as the extension is only implemented in this new part of libhwui and revoking it allows proper usage of the device
    * Related commit in frameworks_base: aosp-mirror/platform_frameworks_base@26a2b97
    * Fixes this:
    	W/Adreno-EGL(  944): <qeglDrvAPI_eglGetConfigAttrib:607>: EGL_BAD_ATTRIBUTE
    
    Change-Id: I7bb069e1cecd721ef10e7b9bd0535ceaa8e91ac0

--- extract-files.sh
+++ extract-files.sh
@@ -72,13 +72,11 @@ CAMERA2_SENSOR_MODULES=$BLOB_ROOT/vendor/lib/libmmcamera2_sensor_modules.so
 sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
 
 # Hax for disable colorspace
-EGL_BLOB_1="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/egl/eglSubDriverAndroid.so 
-EGL_BLOB_2="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/egl/eglsubAndroid.so 
-EGL_BLOB_3="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/egl/libRBEGL_adreno.so 
-EGL_BLOB_4="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/egl/eglSubDriverAndroid.so 
-EGL_BLOB_5="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/egl/eglsubAndroid.so 
-EGL_BLOB_6="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/egl/libRBEGL_adreno.so
-
-sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" "$EGL_BLOB_1" "$EGL_BLOB_2" "$EGL_BLOB_3" "$EGL_BLOB_4" "$EGL_BLOB_5" "$EGL_BLOB_6"
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" $BLOB_ROOT/vendor/lib/egl/eglSubDriverAndroid.so
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" $BLOB_ROOT/vendor/lib/egl/eglsubAndroid.so
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" $BLOB_ROOT/vendor/lib/egl/libRBEGL_adreno.so
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" $BLOB_ROOT/vendor/lib64/egl/eglSubDriverAndroid.so
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" $BLOB_ROOT/vendor/lib64/egl/eglsubAndroid.so
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" $BLOB_ROOT/vendor/lib64/egl/libRBEGL_adreno.so
 
 "$MY_DIR"/setup-makefiles.sh
