3422253a40f2763b1f202c72dfa133b25c1dd06c
Haran Govindan <32816717+YourDeathWish@users.noreply.github.com>
2019-01-12 15:26:32 +0530

    rolex: Add A HEX to Disable colorspace
    * fixes EGL_BAD_ATTRIBUTE
    * occurs only for legacy devices
    * we are getting this error due new implementation in libhwui called EGLExtentions.gl.colorspace
    * which applies additional attributes which misbhaves with our egl libs
    * causes some display werid issues
    
    Tested: fixes some lags,glitches and spams in log
    
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,8 +60,6 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
-"$MY_DIR"/setup-makefiles.sh
-
 # Define blobs path for the following hax
 BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
@@ -72,3 +70,15 @@ sed -i "s|android.hardware.gnss@1.0.so|android.hardware.gnss@1.0-v27.so|g" $BLOB
 # Hax for camera configs
 CAMERA2_SENSOR_MODULES=$BLOB_ROOT/vendor/lib/libmmcamera2_sensor_modules.so
 sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
+
+# Hax for disable colorspace
+EGL_BLOB_1="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/egl/eglSubDriverAndroid.so 
+EGL_BLOB_2="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/egl/eglsubAndroid.so 
+EGL_BLOB_3="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/egl/libRBEGL_adreno.so 
+EGL_BLOB_4="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/egl/eglSubDriverAndroid.so 
+EGL_BLOB_5="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/egl/eglsubAndroid.so 
+EGL_BLOB_6="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/egl/libRBEGL_adreno.so
+
+sed -i "s|EGL_KHR_gl_colorspace|DIS_ABL_ED_colorspace|g" "$EGL_BLOB_1" "$EGL_BLOB_2" "$EGL_BLOB_3" "$EGL_BLOB_4" "$EGL_BLOB_5" "$EGL_BLOB_6"
+
+"$MY_DIR"/setup-makefiles.sh
