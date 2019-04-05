98a72221e8c31f1a9f294b221f987475f621a0ae
najahiiii <tlogitechnjhi@gmail.com>
2019-04-05 17:41:02 +0700

    Syberia

--- syberia_rolex.mk
+++ syberia_rolex.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/syberia/common.mk)
 
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
@@ -29,11 +29,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := syberia_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := rolex
+
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="rolex" \
