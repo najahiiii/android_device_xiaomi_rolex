4bf91a424db0fe0920ecee62f2e8acf617d5167d
najahiiii <tlogitechnjhi@gmail.com>
2019-04-08 16:32:40 +0700

    rolex: add dalvik vm values
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>

---
+++ product/dalvik.mk
@@ -0,0 +1,13 @@
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-threads=4 \
+    dalvik.vm.image-dex2oat-threads=4 \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed
