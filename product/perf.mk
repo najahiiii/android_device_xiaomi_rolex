e4a0fea1d400a594094880943775b309e6f62a16
najahiiii <tlogitechnjhi@gmail.com>
2019-04-08 16:38:15 +0700

    rolex: decrease memory trimming values
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>

--- product/perf.mk
+++ product/perf.mk
@@ -5,4 +5,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.empty_app_percent=50 \
     ro.vendor.qti.sys.fw.trim_empty_percent=100 \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-    ro.vendor.qti.sys.fw.trim_enable_memory=6442450944
+    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
