df3cbd4277de15885ff6b4e050361c9415e77a4f
najahiiii <tlogitechnjhi@gmail.com>
2019-04-08 16:34:21 +0700

    rolex: add B Services adj transition
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>

---
+++ product/bservice.mk
@@ -0,0 +1,5 @@
+#Enable B service
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.vendor.qti.sys.fw.bservice_limit=5 \
+    ro.vendor.qti.sys.fw.bservice_age=5000
