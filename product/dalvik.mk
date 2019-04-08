3bd76dc5404de315580428f418172ed179055f46
najahiiii <tlogitechnjhi@gmail.com>
2019-04-08 16:36:27 +0700

    rolex: add fw dex2oat optimize
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>

--- product/dalvik.mk
+++ product/dalvik.mk
@@ -11,3 +11,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.image-dex2oat-threads=4 \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.image-dex2oat-filter=speed
+
+# FW Optimize
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=4
